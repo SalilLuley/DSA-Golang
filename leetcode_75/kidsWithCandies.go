25aae18f91a4b81e81c95a290dfc15c768f6e3cf
Salil Luley <salil.luley@gmail.com>
2025-01-12 14:01:56 +0000

    Kids with candies

---
+++ leetcode_75/kidsWithCandies.go
@@ -0,0 +1,31 @@
+package leetcode_75
+
+import "fmt"
+
+func KidsWithCandies() {
+	candies := []int{2, 3, 5, 1, 3}
+	extraCandies := 3
+	fmt.Printf("kidsWithCandies(): %v\n", kidsWithCandies(candies, extraCandies))
+}
+
+// Brute force approach
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+func kidsWithCandies(candies []int, extraCandies int) []bool {
+	max := 0
+	ans := make([]bool, len(candies))
+	for _, v := range candies {
+		if v > max {
+			max = v
+		}
+	}
+	for _, v := range candies {
+		if v+extraCandies < max {
+			ans = append(ans, false)
+		} else {
+			ans = append(ans, true)
+		}
+	}
+
+	return ans
+}
--- main/main.go
+++ main/main.go
@@ -3,8 +3,9 @@ package main
 import "example.com/leetcode_75"
 
 func main() {
-	// leetcode_75.TwoSum()
+	leetcode_75.TwoSum()
 	leetcode_75.Fibonacci()
-	// leetcode_75.MergeStrings()
+	leetcode_75.MergeStrings()
 	leetcode_75.GreatestCommonDivisorOfStrings()
+	leetcode_75.KidsWithCandies()
 }
