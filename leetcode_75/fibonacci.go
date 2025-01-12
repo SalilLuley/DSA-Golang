18c12fa21f8e9394747b0058683b06830617b5fb
Salil Luley <salil.luley@gmail.com>
2025-01-12 13:19:10 +0000

    Added fibonacci solution

--- leetcode_75/fibonacci.go
+++ leetcode_75/fibonacci.go
@@ -1,5 +1,25 @@
 package leetcode_75
 
+import "fmt"
+
+// Time complexity: O(n)
+// Space complexity: O(1)
 func Fibonacci() {
-	// fmt.Println("Fibonacci")
+	x := 8
+	fmt.Printf("fibonacci(): %v\n", fibonacci(x))
+}
+
+func fibonacci(x int) int {
+	if x == 0 {
+		return 0
+	} else if x == 1 {
+		return 1
+	} else if x == 2 {
+		return 1
+	}
+	a, b := 0, 1
+	for i := 2; i < x; i++ {
+		a, b = b, a+b
+	}
+	return b
 }
--- main/main.go
+++ main/main.go
@@ -1,12 +1,10 @@
 package main
 
-import (
-	"example.com/leetcode_75"
-)
+import "example.com/leetcode_75"
 
 func main() {
 	// leetcode_75.TwoSum()
-	// leetcode_75.Fibonacci()
+	leetcode_75.Fibonacci()
 	// leetcode_75.MergeStrings()
 	leetcode_75.GreatestCommonDivisorOfStrings()
 }
