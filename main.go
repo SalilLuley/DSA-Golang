a8c00e3c69f944ddd21e6a469cfbc50415806ecd
Salil Luley <salilluley@Salils-MacBook-Air.local>
2025-02-02 22:27:47 +0000

    Added odd even

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	arrayString "example.com/leetcode_75/ArrayString"
 	hashmapOrSet "example.com/leetcode_75/HashmapOrSet"
+	linkedList "example.com/leetcode_75/LinkedList"
 	prefixSum "example.com/leetcode_75/PrefixSum"
 	slidingWindow "example.com/leetcode_75/SlidingWindow"
 	stack "example.com/leetcode_75/Stack"
@@ -37,4 +38,5 @@ func main() {
 	hashmapOrSet.EqualRowColumnPairs()
 	stack.RemovingStarsFromAString()
 	stack.AsteroidCollision()
+	linkedList.OddEvenList()
 }
