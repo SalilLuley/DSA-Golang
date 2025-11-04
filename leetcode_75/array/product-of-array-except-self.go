2bf858442fdb6c28a5fcb338bfd21af35f6c50c3
SalilLuley <salil.luley@gmail.com>
2025-11-04 10:07:30 +0000

    sums

--- leetcode_75/array/product-of-array-except-self.go
+++ leetcode_75/array/product-of-array-except-self.go
@@ -1,6 +1,8 @@
 package array
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // [1 2 3 4]
 // 2 * 3 * 4, 1 * 3 * 4, 1 * 2 * 4, 1 * 2 * 3
@@ -8,17 +10,57 @@ import "fmt"
 
 func ProductExceptSelf() {
 	nums := []int{-1, 1, 0, -3, 3}
-	fmt.Printf("productExceptSelf() %v", productExceptSelf(nums))
+	fmt.Printf("productExceptSelf() %v\n", productExceptSelf(nums))
 }
 
+// Does not work with 0
+// func productExceptSelf(nums []int) []int {
+// 	sum := 1
+// 	results := []int{}
+// 	for _, val := range nums {
+// 		sum *= val
+// 	}
+// 	for _, val := range nums {
+// 		results = append(results, sum/val)
+// 	}
+// 	return results
+// }
+
+// Left
+// prefix - 1
+// res - [1, 1, 2, 6]
+// 1*1, 1*2 = 2 , 2 * 3 = 6, 6 * 6 = 36
+
+// right
+// nums[1,2,3,4]
+// res[1,1,2,6]
+// suffix - 1/4/12/24
+// res[i] = 6*1, 2*4, 12*1, 24*1
+// suffix = 1*4, 4*3, 12*2,
+// pos - 3
+
+// -1,1,0,-3,3
+// prefix - 1/-1/-1/0/0/0
+// res = [1, -1, -1, 0, 0, 0]
+// prefix = 1*-1, -1*1, -1*0, 0*-3,0*3
+
+//suffix - 1
+//res = i ==
+
 func productExceptSelf(nums []int) []int {
-	sum := 1
-	results := []int{}
+	res := []int{}
+	prefix := 1
 	for _, val := range nums {
-		sum *= val
+		res = append(res, prefix)
+		prefix *= val
 	}
-	for _, val := range nums {
-		results = append(results, sum/val)
+
+	suffix := 1
+
+	for i := len(nums) - 1; i >= 0; i-- {
+		res[i] *= suffix
+		suffix *= nums[i]
 	}
-	return results
+
+	return res
 }
